992fa28ec6eeed51366a949618256cf279687489
Jack Furci <furci.j@northeastern.edu>
2023-03-05 14:15:03 -0500

    formatting

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -5,21 +5,21 @@ import WhoToFollowList from "./who-to-follow-list";
 import PostSummaryList from "./post-summary-list";
 
 function Tuiter() {
- return (
-   <div className="row mt-2">
-     <div className="col-2 col-md-2 col-lg-1 col-xl-2">
-       <NavigationSidebar active="explore"/>
-     </div>
-     <div className="col-10 col-md-10 col-lg-7 col-xl-6 mb-2"
-          style={{"position": "relative"}}>
-       <ExploreComponent/>
-       <PostSummaryList />
-     </div>
-     <div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
-       <WhoToFollowList/>
-     </div>
-   </div>
- );
+  return (
+    <div className="row mt-2">
+      <div className="col-2 col-md-2 col-lg-1 col-xl-2">
+        <NavigationSidebar active="explore" />
+      </div>
+      <div className="col-10 col-md-10 col-lg-7 col-xl-6 mb-2"
+        style={{ "position": "relative" }}>
+        <ExploreComponent />
+        <PostSummaryList />
+      </div>
+      <div className="d-none d-xxl-block d-xl-block d-lg-block col-xxl-4 col-xl-4 col-lg-4">
+        <WhoToFollowList />
+      </div>
+    </div>
+  );
 }
 
 export default Tuiter;
--- src/tuiter/navigation-sidebar/index.js
+++ src/tuiter/navigation-sidebar/index.js
@@ -7,38 +7,38 @@ const NavigationSidebar = (
     return (
         <div className="list-group">
             <a className="list-group-item">
-                <i class="fab fa-twitter"></i> Tuiter</a>
+                <i class="fab fa-twitter"></i><span class="d-none d-xl-inline d-xxl-inline"> Tuiter</span></a>
             <a className={`list-group-item
                          ${active === 'home' ? 'active' : ''}`}>
-                <i class="fa fa-home"></i> Home
+                <i class="fa fa-home"></i><span class="d-none d-xl-inline d-xxl-inline"> Home</span>
             </a>
             <a className={`list-group-item
                          ${active === 'explore' ? 'active' : ''}`}>
-                <i class="fa fa-hashtag"></i> Explore
+                <i class="fa fa-hashtag"></i><span class="d-none d-xl-inline d-xxl-inline"> Explore</span>
             </a>
             <a className={`list-group-item
                          ${active === 'notifications' ? 'active' : ''}`}>
-                <i class="fa fa-bell"></i> Notifications
+                <i class="fa fa-bell"></i><span class="d-none d-xl-inline d-xxl-inline"> Notifications</span>
             </a>
             <a className={`list-group-item
                          ${active === 'messages' ? 'active' : ''}`}>
-                <i class="fa fa-envelope"></i> Messages
+                <i class="fa fa-envelope"></i><span class="d-none d-xl-inline d-xxl-inline"> Messages</span>
             </a>
             <a className={`list-group-item
                          ${active === 'bookmarks' ? 'active' : ''}`}>
-                <i class="fa fa-bookmark"></i> Bookmarks
+                <i class="fa fa-bookmark"></i><span class="d-none d-xl-inline d-xxl-inline"> Bookmarks</span>
             </a>
             <a className={`list-group-item
                          ${active === 'lists' ? 'active' : ''}`}>
-                <i class="fa fa-list"></i> Lists
+                <i class="fa fa-list"></i><span class="d-none d-xl-inline d-xxl-inline"> Lists</span>
             </a>
             <a className={`list-group-item
                     ${active === 'profile' ? 'active' : ''}`}>
-                <i class="fa fa-user"></i> Profile
+                <i class="fa fa-user"></i><span class="d-none d-xl-inline d-xxl-inline"> Profile</span>
             </a>
             <a className={`list-group-item
                     ${active === 'more' ? 'active' : ''}`}>
-                <i class="fa fa-circle"></i> More
+                <i class="fa fa-circle"></i><span class="d-none d-xl-inline d-xxl-inline"> More</span>
             </a>
         </div>
     );
